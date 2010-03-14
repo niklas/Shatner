ed49270bba5d64307b69ed0ccdd40e02d4984bf4
Niklas Hofer <niklas+dev@lanpartei.de>
2010-03-14 16:22:05 +0100

    optional attributes

--- lib/shatner/helpers/attributes.rb
+++ lib/shatner/helpers/attributes.rb
@@ -2,7 +2,7 @@ module Shatner
   module Helpers
     # This methods can be called from a template
     module Attributes
-      def string(attribute_name)
+      def string(attribute_name, opts={})
         item(attribute_name) do
           text record.send(attribute_name).to_s
         end
