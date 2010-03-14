7ea5aec746d771528a742f44c9094497b4134c4a
Niklas Hofer <niklas+dev@lanpartei.de>
2010-03-14 18:57:16 +0100

    Edit works for new and existing records - may change later, we will see

--- spec/shatner/widgets/record/edit_spec.rb
+++ spec/shatner/widgets/record/edit_spec.rb
@@ -13,13 +13,38 @@ module Shatner::Record
       @widget.stub!(:parent).and_return(@actionview)
     end
 
-    it "should render a frame with title and form" do
+    it "should render a frame with title and form to create document" do
+      @document.stub!(:new_record?).and_return(true)
+      @actionview.should_receive(:polymorphic_path).and_return('/documents')
+      @widget.to_s.should be_html_with do
+        div :class => 'frame' do
+          h3 'edit "A Document"', :class => 'title'
+          form :action => '/documents', :method => 'post', :class => 'new_document' do
+            h4 'General', :class => 'group_head'
+            dl :class => 'group' do
+              di :class => 'title' do
+                dt do 
+                  label 'Title', :for => 'document_title'
+                end
+                dd do
+                  input :value => 'A Document', :type => 'text', :id => 'document_title'
+                end
+              end
+            end
+          end
+        end
+      end
+      
+    end
+
+    it "should render a frame with title and form to update document" do
+      @document.stub!(:new_record?).and_return(false)
       @actionview.should_receive(:polymorphic_path).and_return('/documents/1')
       @widget.to_s.should be_html_with do
         div :class => 'frame' do
           h3 'edit "A Document"', :class => 'title'
-          form :action => '/documents/1', :method => 'post', :class => 'new_document' do
-            # TODO _method
+          form :action => '/documents/1', :method => 'post', :class => 'edit_document' do
+            input :name => '_method', :value => 'put'
             h4 'General', :class => 'group_head'
             dl :class => 'group' do
               di :class => 'title' do
